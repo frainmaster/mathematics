24edd85f024087608c3e639f6b09cbf5e1ab573f
Irfan Ahmad <32104766+frainmaster@users.noreply.github.com>
2018-08-24 11:08:15 +0800

    Update prime factor.py

--- prime factor/prime factor.py
+++ prime factor/prime factor.py
@@ -1,14 +1,17 @@
 num = 1
 while(num != 0):
-    print("put a number to break it down to its prime factors")
-    num = int(input())
-    i = 2
-    fact = []
-    while (i<=num):
-        if(num%i==0):
-            num /= i
-            fact.append(i)
-        else:
-            i += 1
-    
-    print(str(fact)[1:len(str(fact))-1])
+    print("put a positive integer to break it down to its prime factors")
+    num = input()
+    if str(num).isdigit() and str(num)[0] != "-":
+        num = int(num)
+        i = 2
+        fact = []
+        while (i<=num):
+            if(num%i==0):
+                num /= i
+                fact.append(i)
+            else:
+                i += 1
+        print(str(fact)[1:len(str(fact))-1] + '\n')
+    else:
+        print('number is not a positive integer' + '\n')
